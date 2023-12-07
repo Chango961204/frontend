708cfc1db2b24f06419c2187327178b60140a107
Chango961204 <metallicaluis73@gmail.com>
2023-12-07 15:41:05 -0600

    ajustes ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,39 +2,49 @@ import React from "react";
 import { useProducts } from "../context/ProductsContext";
 import { Link } from "react-router-dom";
 import { IoTrashBinSharp, IoPencilSharp } from "react-icons/io5";
+import { format } from "date-fns";
 
 function ProductCard({ product }) {
+    const { deleteProduct } = useProducts();
 
-    const { deleteProduct } = useProducts()
+    const formattedCreatedAt = format(new Date(product.createdAt), "MMMM dd, yyyy HH:mm:ss");
+    const formattedUpdatedAt = format(new Date(product.updatedAt), "MMMM dd, yyyy HH:mm:ss");
 
     return (
-        <div className='bg-zinc-800 max-w-md w-full p-10 rounded-lg'>
-            <header className='felx justify-between'>
-                <h1 className='text-1xl font-bold'>{product.name}</h1>
+        <div className='bg-zinc-800 max-w-md w-full p-6 rounded-lg shadow-md'>
+            <header className='flex justify-between items-center mb-4'>
+                <h1 className='text-2xl font-bold'>{product.name}</h1>
                 <div className='flex gap-x-2 items-center'>
-                    <button className='bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-xl'
+                    <button
+                        className='bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded-md'
                         onClick={() => {
-                            //console.log(product._id);
                             deleteProduct(product._id);
                         }}
                     >
                         <IoTrashBinSharp />
                     </button>
-                    <Link to={'/products/' + product._id}
-                        className='bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-xl'
+                    <Link
+                        to={'/products/' + product._id}
+                        className='bg-green-500 hover:bg-green-600 text-white px-3 py-1 rounded-md'
                     >
                         <IoPencilSharp />
                     </Link>
                 </div>
             </header>
             <p className='text-slate-300 my-2'>
-                {product.price}
+                <span className="font-bold">Precio:</span> {product.price}
             </p>
             <p className='text-slate-300 my-2'>
-                {product.year}
+                <span className="font-bold">Año:</span> {product.year}
+            </p>
+            <p className='text-slate-300 my-2'>
+                <span className="font-bold">Fecha de Creación:</span> {formattedCreatedAt}
+            </p>
+            <p className='text-slate-300 my-2'>
+                <span className="font-bold">Fecha de Modificación:</span> {formattedUpdatedAt}
             </p>
         </div>
-    )
+    );
 }
 
-export default ProductCard
+export default ProductCard;
